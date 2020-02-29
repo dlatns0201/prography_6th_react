579941bacf8f6dcf4f5bdf82877fefd665738f74
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-01 03:50:48 +0900

    chore: typescript config setting
    eslint-airbnb와 prettier 설정
    tsconfig 설정

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly'
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				paths: ['src'],
+				extensions: ['.d.ts', '.ts', '.tsx']
+			}
+		}
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: { 'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }] }
+};
