122e2751db453e1a0078e97d4f0908cb919e0771
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-02 01:07:18 +0900

    style: compose app layout
    자식 컴포넌트에서 100vw를 사용해서 생기는 가로 스크롤을 없애기 위해 body 태그 margin을 없앰
    root의 자식 컴포넌트를 AppContainer란 wrapper를 추가한 뒤, 기본 배경과 레이아웃을 화면 크기에 맞게 지정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 
 import Navigation from './components/organisms/Navigation';
 import MoviePage from './pages/Movie';
 import TodoPage from './pages/Todo';
 
+const AppContainer = styled.div`
+	background-color: #ecf0f1;
+	width: 100vw;
+	min-height: 100vh;
+`;
+
 const App = () => {
 	return (
-		<>
+		<AppContainer>
 			<Navigation />
 			<Switch>
 				<Route path="/movie" component={MoviePage} />
 				<Route exact path="/" component={TodoPage} />
 				<Route path="/" render={() => <div>404 Not Found</div>} />
 			</Switch>
-		</>
+		</AppContainer>
 	);
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import App from './App';
 
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+    }
+`;
+
 ReactDOM.render(
 	<BrowserRouter>
+		<GlobalStyle />
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
