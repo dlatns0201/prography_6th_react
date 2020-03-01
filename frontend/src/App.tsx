9bdd510db2607dad9a1bbef691f999bbf435872d
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-01 23:46:58 +0900

    feat: write app, index
    app은 navigation(organism), Page들로 구성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Navigation from './components/organisms/Navigation';
+import MoviePage from './pages/Movie';
+import TodoPage from './pages/Todo';
 
 const App = () => {
-	return <div>Hello Prography</div>;
+	return (
+		<>
+			<Navigation />
+			<Switch>
+				<Route path="/movie" component={MoviePage} />
+				<Route exact path="/" component={TodoPage} />
+				<Route path="/" render={() => <div>404 Not Found</div>} />
+			</Switch>
+		</>
+	);
 };
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
+);
