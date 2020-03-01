c2d3830d1a79d8be0fd56a6c14f4f4f2734b46b2
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-01 22:00:08 +0900

    feat: write button of atoms

--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 interface ButtonProps {
 	children: React.ReactChildren;
+	flex: number | 'auto';
+	color: string;
+	size: 'small' | 'normal' | 'big';
 }
 
-const StyledButton = styled.button``;
+const Size = {
+	small: ['7px', '7px', '1rem'],
+	normal: ['10px', '10px', '1.2rem'],
+	big: ['14px', '14px', '1.4rem']
+} as const;
 
-const Button = ({ children }: ButtonProps) => {
-	return <StyledButton>{children}</StyledButton>;
+const StyledButton = styled.button`
+	border: none;
+	background-color: transparent;
+	flex: ${(props: ButtonProps) => props.flex};
+	color: ${(props: ButtonProps) => props.color};
+	${(props: ButtonProps) => css`
+		padding: ${Size[props.size][0]} ${Size[props.size][1]};
+		font-size: Size[props.size][2];
+	`}
+`;
+
+const Button = ({ children, flex = 'auto', color = 'black', size = 'normal' }: ButtonProps) => {
+	return (
+		<StyledButton flex={flex} color={color} size={size}>
+			{children}
+		</StyledButton>
+	);
 };
 
 export default Button;
