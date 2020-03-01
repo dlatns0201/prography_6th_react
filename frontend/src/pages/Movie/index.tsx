a487f79c5a30ba98e46786668422a03c1651953d
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-01 23:40:39 +0900

    feat: write initial code of page and templates for movie

---
+++ frontend/src/pages/Movie/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledMoviePage = styled.div``;
+
+const MoviePage = () => {
+	return <StyledMoviePage />;
+};
+
+export default MoviePage;
