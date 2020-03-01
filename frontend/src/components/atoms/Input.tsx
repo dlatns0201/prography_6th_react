4908c8ca4dc603d540ac60ebdbb1d829464d5db6
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-02 03:24:21 +0900

    style: add input style for todo with classnames

--- frontend/src/components/atoms/Input.tsx
+++ frontend/src/components/atoms/Input.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
+import cn from 'classnames';
 
 interface InputProps {
 	children?: React.ReactChildren;
 	placeholder?: string;
+	todo?: boolean;
 }
 
-const StyledInput = styled.span`
+const StyledInput = styled.input<InputProps>`
+	display: inline-block;
 	flex: 1;
+	border: 0.3px solid lightgray;
+
+	&.todo {
+		flex-grow: 1;
+		min-height: 2.3em;
+		font-size: 1.8rem;
+		padding: 0 1em;
+	}
 `;
 
-const Input = ({ children, placeholder }: InputProps) => {
-	return <StyledInput placeholder={placeholder}>{children}</StyledInput>;
+const Input = ({ children, placeholder, todo }: InputProps) => {
+	return (
+		<StyledInput className={cn({ todo })} placeholder={placeholder}>
+			{children}
+		</StyledInput>
+	);
 };
 
 export default Input;
