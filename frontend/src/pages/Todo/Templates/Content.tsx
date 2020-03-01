6ce2e518be8859a9138083244b3d4572ac76304d
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-01 23:41:59 +0900

    feat: write initial code of page, template for todo

---
+++ frontend/src/pages/Todo/Templates/Content.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledContent = styled.div``;
+
+const Content = () => {
+	return <StyledContent />;
+};
+
+export default Content;
---
+++ frontend/src/pages/Todo/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTodoPage = styled.div``;
+
+const TodoPage = () => {
+	return <StyledTodoPage />;
+};
+
+export default TodoPage;
