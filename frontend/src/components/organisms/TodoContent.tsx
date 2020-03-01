9d2ebcd4ff6c4646d24bd750cd571824429236f0
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-02 03:22:06 +0900

    feat: write todo content(organism)

--- frontend/src/components/organisms/TodoContent.tsx
+++ frontend/src/components/organisms/TodoContent.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import Title from '../molecules/Title';
+import Form from '../atoms/Form';
+import List from '../molecules/List';
+import Input from '../atoms/Input';
+import ListItem from '../molecules/ListItem';
 
 interface TodoContentProps {}
 
 const StyledTodoContent = styled.div`
-	min-width: 60%;
+	min-width: 40vw;
+	display: flex;
+	flex-direction: column;
 `;
 
 const TodoContent = () => {
 	return (
 		<StyledTodoContent>
-			<Title color="#FDA7DF">Todos</Title>
+			<Title color="#FDA7DF" fontSize="3rem">
+				Todos
+			</Title>
+			<Form flexDirection="column">
+				<Input placeholder="무엇을 해야하나요?" todo />
+			</Form>
+			<List white>
+				<ListItem>abc</ListItem>
+				<ListItem>123</ListItem>
+			</List>
 		</StyledTodoContent>
 	);
 };
