586817aa0c22a8619b67b83f2a899c44de71afc4
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-04 23:38:22 +0900

    chore: modify webpack config file to dynamic env divided by multiple config files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,31 +1,6 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-	mode: 'development',
-	entry: './src/index.tsx',
-	devServer: {
-		historyApiFallback: true,
-		inline: true,
-		port: 3000,
-		hot: true,
-		publicPath: '/'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: 'awesome-typescript-loader'
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.js', 'jsx', '.ts', '.tsx']
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new HtmlWebpackPlugin({
-			template: 'public/index.html'
-		})
-	]
+/* eslint-disable func-names */
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+module.exports = function(env) {
+	return require(`./webpack.${env}.js`);
 };
