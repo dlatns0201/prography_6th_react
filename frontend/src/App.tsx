1e98085986a32531e4f6ff6fd148c80573903434
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-15 16:28:27 +0900

    feat: move store provider from app to index
    App에서 사용자 정보를 가져오는 dispatch를 하기 위해서
    또한, index에서 provider 설정하는 것이 더 올바르다고 생각하기 때문

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import styled from 'styled-components';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createSaga from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { useSelector, useDispatch } from 'react-redux';
 // import loadable from '@loadable/component';
 
-import rootReducer from './modules';
-import rootSaga from './sagas';
+import { loadUserInfoRequest } from './modules/user';
 import Navigation from './components/organisms/Navigation';
 import MoviePage from './pages/Movie';
 import TodoPage from './pages/Todo';
 import SignupPage from './pages/Signup';
 import LoginPage from './pages/Login';
+import { RootState } from './modules';
 // const Navigation = loadable(() => import('./components/organisms/Navigation'));
 // const MoviePage = loadable(() => import('./pages/Movie'));
 // const TodoPage = loadable(() => import('./pages/Todo'));
 
-const sagaMiddleware = createSaga();
-const middlewares = [sagaMiddleware];
-const enhancer = compose(composeWithDevTools(applyMiddleware(...middlewares)));
-const store = createStore(rootReducer, undefined, enhancer);
-sagaMiddleware.run(rootSaga);
-
 const App = () => {
+	const { userInfo } = useSelector((state: RootState) => state.user);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (!userInfo) dispatch(loadUserInfoRequest());
+	}, [userInfo]);
+
 	return (
-		<Provider store={store}>
+		<>
 			<Navigation />
 			<Switch>
 				<Route path="/movie" component={MoviePage} />
@@ -35,7 +32,7 @@ const App = () => {
 				<Route exact path="/signup" component={SignupPage} />
 				<Route path="/" render={() => <div>404 Not Found</div>} />
 			</Switch>
-		</Provider>
+		</>
 	);
 };
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
-import { hydrate } from 'react-dom';
-import { loadableReady } from '@loadable/component';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSaga from 'redux-saga';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer, { RootState } from './modules';
+import rootSaga from './sagas';
+// import { hydrate } from 'react-dom';
+// import { loadableReady } from '@loadable/component';
 
 import App from './App';
 
@@ -24,11 +31,19 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
+const sagaMiddleware = createSaga();
+const middlewares = [sagaMiddleware];
+const enhancer = compose(composeWithDevTools(applyMiddleware(...middlewares)));
+const store = createStore(rootReducer, undefined, enhancer);
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(
-	<BrowserRouter>
-		<GlobalStyle />
-		<App />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<GlobalStyle />
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root')
 );
 
