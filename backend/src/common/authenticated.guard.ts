0e4ad4c67b7c036972c8987c1005a0d109fb37f1
Lim Tae Hyun <dlatns0201@naver.com>
2020-03-15 14:48:00 +0900

    feat: write login guard
    login 라우트로 가게 되면 처음으로 login.guard 로직이 실행됨
      super.activate에서 local.stategy 가 실행되어 true한 값이 발생되어야 다음으로 넘어가게 함
    authenticated.guard는 로그인 상태인지 아닌지 확인하는 미들웨어

---
+++ backend/src/common/authenticated.guard.ts
@@ -0,0 +1,9 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+
+@Injectable()
+export class AuthenticatedGuard implements CanActivate {
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    return request.isAuthenticated();
+  }
+}
